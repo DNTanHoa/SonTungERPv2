434f4a49a5aad4f2e0769a349a473a75624b1eca
dinhvanluan <dinhvanluan.tn@gmail.com>
2021-01-31 23:01:30 +0700

    Translate dateedit

--- SonTungERP.Blazor.Server/Startup.cs
+++ SonTungERP.Blazor.Server/Startup.cs
@@ -94,6 +94,15 @@ namespace SonTungERP.Blazor.Server {
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            var supportedCultures = new[]{"vi-VN"};
+            var localizationOptions = new RequestLocalizationOptions()
+                .SetDefaultCulture(supportedCultures[0])
+                .AddSupportedCultures(supportedCultures)
+                .AddSupportedUICultures(supportedCultures);
+
+            app.UseRequestLocalization(localizationOptions);
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseRouting();
